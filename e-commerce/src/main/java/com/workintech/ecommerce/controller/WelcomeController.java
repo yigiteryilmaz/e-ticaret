8d5fbe3dc5e509443103b2f30213a72b5dc179c7
yigiteryilmaz <nyigiteryilmaz@hotmail.com>
2025-01-14 22:43:24 +0300

    Update WelcomeController.java

--- e-commerce/src/main/java/com/workintech/ecommerce/controller/WelcomeController.java
+++ e-commerce/src/main/java/com/workintech/ecommerce/controller/WelcomeController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping
     public String hi(){
-        return "welcome hi deneme ";
+        return "Merhaba, hoşgeldiniz:)";
     }
 }
